1871c5d46f588aed6e1f648bfcd80420a69eb882
hjg727 <tony9800@naver.com>
2025-06-02 16:02:35 +0900

    init

---
+++ src/main/java/ureca/muneobe/MuneoBeApplication.java
@@ -0,0 +1,13 @@
+package ureca.muneobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuneoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuneoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ureca/muneobe/MuneoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ureca.muneobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuneoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
