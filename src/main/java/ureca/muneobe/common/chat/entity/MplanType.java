38c056f9a26979bbda2ce09aa4d193ef37b69295
sangyunpark99 <psangyun5108@gmail.com>
2025-06-09 18:36:55 +0900

    MUN-21 feat: MplanType 구현

--- src/main/java/ureca/muneobe/common/chat/entity/MplanType.java
+++ src/main/java/ureca/muneobe/common/chat/entity/MplanType.java
@@ -1,5 +1,14 @@
 package ureca.muneobe.common.chat.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MplanType {
-    LTE, _5G
+    LTE_5G("5G_LTE 모바일 요금제"),
+    NUGGET("USIM/ESIM 전용 모바일 요금제"),
+    SMART_DEVICE("스마트기기/테블릿 전용 모바일 요금제");
+
+    private final String description;
 }
