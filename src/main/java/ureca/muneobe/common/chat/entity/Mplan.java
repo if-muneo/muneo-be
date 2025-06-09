28e2369247fe3b36f1abdcb033816fd39b2310ba
jeongjiho <wjdwlgh2000@naver.com>
2025-06-09 14:41:42 +0900

    MUN-11 fix:엔티티 매핑 에러 고침

--- src/main/java/ureca/muneobe/common/chat/entity/Mplan.java
+++ src/main/java/ureca/muneobe/common/chat/entity/Mplan.java
@@ -40,9 +40,9 @@ public class Mplan {
     @JoinColumn(name = "mplan_detail_id")
     private MplanDetail mplanDetail;
 
-    @OneToMany(mappedBy = "")
+    @OneToMany(mappedBy = "mplan")
     List<Review> reviews = new ArrayList<>();
 
-    @OneToMany(mappedBy = "")
+    @OneToMany(mappedBy = "mplan")
     List<Subscription> subscriptions = new ArrayList<>();
 }
