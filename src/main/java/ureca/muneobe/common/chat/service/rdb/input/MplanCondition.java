940e3ce11ac5b8db662cb94d5c98f0a15829af4c
sangyunpark99 <psangyun5108@gmail.com>
2025-06-11 13:27:51 +0900

    MUN-21 feat: MplanCondition 필드 변수 추가

--- src/main/java/ureca/muneobe/common/chat/service/rdb/input/MplanCondition.java
+++ src/main/java/ureca/muneobe/common/chat/service/rdb/input/MplanCondition.java
@@ -1,17 +1,23 @@
 package ureca.muneobe.common.chat.service.rdb.input;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import ureca.muneobe.common.chat.entity.DataType;
 import ureca.muneobe.common.chat.entity.MplanType;
 import ureca.muneobe.common.chat.entity.Qualification;
 
 @Getter
+@AllArgsConstructor
+@Builder
 public class MplanCondition {
     private String name;
+    private Range monthlyPrice;
+    private Range dailyData;
     private Range basicDataAmount;
     private Range sharingData;
-    private Range monthlyPrice;
     private Range voiceCallVolume;
+    private Range subDataSpeed;
     private Boolean textMessage;
     private Qualification qualification;
     private DataType dataType;
