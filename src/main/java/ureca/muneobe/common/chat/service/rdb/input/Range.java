b2aaead2b4f0713fbb1b95da31d439eb883b0e87
sangyunpark99 <psangyun5108@gmail.com>
2025-06-11 13:28:55 +0900

    MUN-21 feat: Range클래스 @AllArgsConstructor, @Builder 어노테이션 추가

--- src/main/java/ureca/muneobe/common/chat/service/rdb/input/Range.java
+++ src/main/java/ureca/muneobe/common/chat/service/rdb/input/Range.java
@@ -1,10 +1,14 @@
 package ureca.muneobe.common.chat.service.rdb.input;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Range {
     private Integer baseNumber;
     private Integer subNumber;
