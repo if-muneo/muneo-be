47d6c2f2fe386a87d057e72b8d491f4135b652e4
likerhythm <wjdalsdl0412@gmail.com>
2025-06-11 16:48:20 +0900

    MUN-25 feat: db 읽고 Trie를 rebuild하는 기능

--- src/main/java/ureca/muneobe/slang/service/SlangHolder.java
+++ src/main/java/ureca/muneobe/slang/service/SlangHolder.java
@@ -2,60 +2,49 @@ package ureca.muneobe.slang.service;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.ahocorasick.trie.Emit;
 import org.ahocorasick.trie.Trie;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
+import ureca.muneobe.common.chat.entity.Slang;
 import ureca.muneobe.slang.SlangRepository;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Slf4j
-@Service
+@Component
 @AllArgsConstructor
 public class SlangHolder {
 
     private final SlangRepository slangRepository;
+    private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
 
     private volatile Trie trie;
     private Trie.TrieBuilder trieBuilder;
 
     public int countSlang(String text) {
-        Collection<Emit> emits = trie.parseText(text);
-        return emits.size();
-    }
-
-    public void reset() {
-        this.trie = trieBuilder.build();
-    }
-
-    public void addSlang(String text) {
-        // trie에 text가 존재하는지 확인
-        boolean contain = trie.containsMatch(text);
-        if (contain) {
-            log.info("{}는 이미 비속어로 등록되어 있습니다.", text);
-            return;
-        }
-
-        this.trieBuilder.addKeyword(text);
-    }
-
-    public void deleteSlang(String text) {
-
-    }
-
-    public void addSlang(List<String> texts) {
-        for (String text : texts) {
-            addSlang(text);
+        rwLock.readLock().lock();
+        try {
+            // 여기서 writeLock을 얻으려고 하면 데드락 발생(writeLock은 모든 readLock이 해제되길 기다리기 때문에)
+            return trie.parseText(text).size();
+        } finally {
+            rwLock.readLock().unlock();
         }
     }
 
-    public void deleteSlang(List<String> texts) {
+    public int rebuild() {
+        List<Slang> all = slangRepository.findAll();
+        List<String> slangList = all.stream().map(Slang::getWord).toList();
+        trieBuilder.addKeywords(slangList);
+        Trie newTrie = trieBuilder.build();
+        log.info("{}개의 비속어가 rebuild 되었습니다.", slangList.size());
 
-    }
+        rwLock.writeLock().lock();
+        try{
+            this.trie = newTrie;
+        } finally {
+            rwLock.writeLock().unlock();
+        }
 
-    public boolean contain(String text) {
-//        trie.
-        return false;
+        return slangList.size();
     }
 }
