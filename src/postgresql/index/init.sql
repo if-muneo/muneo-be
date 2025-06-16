656a6ae4b6b61fcbd754eeb5d9c2c7e332171710
sangyunpark99 <psangyun5108@gmail.com>
2025-06-16 15:45:05 +0900

    MUN-38 feat: index 쿼리문 추가

--- src/postgresql/index/init.sql
+++ src/postgresql/index/init.sql
@@ -1,2 +1,3 @@
+CREATE INDEX idx_mplan_detail_monthly_price ON mplan_detail(monthly_price);
 CREATE INDEX idx_addon_group_id ON addon(addon_group_id); -- addon_group_id
 CREATE INDEX idx_addon_name_trgm ON addon USING gin (name gin_trgm_ops); -- addon_name %like% 인덱싱
