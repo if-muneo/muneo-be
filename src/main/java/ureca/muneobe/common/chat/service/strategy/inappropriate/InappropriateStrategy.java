a46fa2e040f37cc8440b4589fd54f98d9fb73d43
sangyunpark99 <psangyun5108@gmail.com>
2025-06-17 19:28:50 +0900

    MUN-47 refactor: 스트리밍 방식을 사용하기 위해서 Mono.just().flux()로 변경

--- src/main/java/ureca/muneobe/common/chat/service/strategy/inappropriate/InappropriateStrategy.java
+++ src/main/java/ureca/muneobe/common/chat/service/strategy/inappropriate/InappropriateStrategy.java
@@ -1,6 +1,7 @@
 package ureca.muneobe.common.chat.service.strategy.inappropriate;
 
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ureca.muneobe.common.chat.dto.result.FirstPromptResult;
 import ureca.muneobe.common.chat.service.strategy.RoutingStrategy;
@@ -8,7 +9,7 @@ import ureca.muneobe.common.chat.service.strategy.RoutingStrategy;
 @Component("inappropriateStrategy")
 public class InappropriateStrategy implements RoutingStrategy {
     @Override
-    public Mono<String> process(FirstPromptResult firstPromptResult) {
-        return Mono.just("부적절한 단어가 감지되었습니다. 다시 질문해주세요.");
+    public Flux<String> process(FirstPromptResult firstPromptResult) {
+        return Mono.just("부적절한 단어가 감지되었습니다. 다시 질문해주세요.").flux();
     }
 }
