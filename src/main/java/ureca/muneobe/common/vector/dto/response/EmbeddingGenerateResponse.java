2d18290eb098691ab5104f867c47194be1563f1e
marineAqu <wii021004@naver.com>
2025-06-18 22:27:29 +0900

    MUN-37 refactor: 불필요 어노테이션 삭제

--- src/main/java/ureca/muneobe/common/vector/dto/response/EmbeddingGenerateResponse.java
+++ src/main/java/ureca/muneobe/common/vector/dto/response/EmbeddingGenerateResponse.java
@@ -3,11 +3,9 @@ package ureca.muneobe.common.vector.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class EmbeddingGenerateResponse {
     private String answer;
