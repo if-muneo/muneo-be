8c0f988ebe4d438d3333a3e2d2c31df4fbfcab95
marineAqu <wii021004@naver.com>
2025-06-19 22:16:03 +0900

    MUN-54 test: test code 작성

--- src/test/java/ureca/muneobe/common/mplan/controller/MplanDetailControllerTest.java
+++ src/test/java/ureca/muneobe/common/mplan/controller/MplanDetailControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class MplanDetailControllerTest {
     @Autowired
     private MockMvc mockMvc;
