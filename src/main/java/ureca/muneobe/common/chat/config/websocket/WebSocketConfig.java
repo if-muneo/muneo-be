fe3ee98f1011e880a7bfe44bcf21803f0c9c29c8
sangyunpark99 <psangyun5108@gmail.com>
2025-06-20 11:48:04 +0900

    MUN-62 chore: 배포 환경 설정

--- src/main/java/ureca/muneobe/common/chat/config/websocket/WebSocketConfig.java
+++ src/main/java/ureca/muneobe/common/chat/config/websocket/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package ureca.muneobe.common.chat.config.websocket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -16,18 +18,30 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .addInterceptors(new UserHandshakeInterceptor())
                 .setHandshakeHandler(new CustomHandshakeHandler())
                 .setAllowedOriginPatterns("*") // 프론트엔드의 origin에 상관없이 /muneo-chat 엔드 포인트에 접근 가능
-                .withSockJS(); // 브라우저가 websocket을 지원하지 않거나 네트워크 환경상 websocket 연결이 불가능할 때
-                               // HTTP 롱폴링이나 폴백 기법으로 연결을 유지하도록 SockJS 라이브러리를 활성화
+                .withSockJS()
+                .setHeartbeatTime(1000); // 브라우저가 websocket을 지원하지 않거나 네트워크 환경상 websocket 연결이 불가능할 때
+        // HTTP 롱폴링이나 폴백 기법으로 연결을 유지하도록 SockJS 라이브러리를 활성화
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         // 클라이언트가 구독할 브로커 주소를 /topic으로 지정
-        config.enableSimpleBroker("/topic", "/queue");
+        config.enableSimpleBroker("/topic", "/queue")
+                .setHeartbeatValue(new long[]{100000000, 1000})
+                .setTaskScheduler(websocketConnectScheduler());
 
         // 클라이언트가 stomp send 시 url 앞에 /chat을 붙여야 스프링의 @MessageMapping으로 들어온다.
         config.setApplicationDestinationPrefixes("/pub");
 
         config.setUserDestinationPrefix("/user");
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler websocketConnectScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("웹소켓 커넥션");
+        scheduler.initialize();
+        return scheduler;
     }
 }
--- src/main/java/ureca/muneobe/common/chat/event/WebSocketEventListener.java
+++ src/main/java/ureca/muneobe/common/chat/event/WebSocketEventListener.java
@@ -1,12 +1,14 @@
 package ureca.muneobe.common.chat.event;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import ureca.muneobe.common.chat.service.ChatService;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class WebSocketEventListener {
@@ -15,6 +17,7 @@ public class WebSocketEventListener {
 
     @EventListener
     public void handleDisconnect(SessionDisconnectEvent event) {
+        log.info("Disconnected from websocket");
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
 
         // 웹소켓 세션에 저장된 사용자 정보 가져오기
